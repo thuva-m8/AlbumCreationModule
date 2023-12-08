bf7aa2cebafa8001b40cb370b5786856c6691e4e
Thuvarakan Mathivannan <thuva@Thuvarakans-iMac.local>
2023-12-08 10:39:19 +0530

    navbar

--- app/navbar.js
+++ app/navbar.js
@@ -39,14 +39,6 @@ export default function navbar() {
                                 </Disclosure.Button>
                             </div>
                             <div className="flex-2 flex items-center justify-center sm:items-stretch sm:justify-start">
-                                <div className="flex-shrink-0 flex items-center">
-                                    <img
-                                        className="block lg:hidden h-8 w-auto"
-                                        src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
-                                        alt="Workflow"
-                                    />
-
-                                </div>
                                 <div
                                     className="hidden sm:ml-6 sm:flex sm:space-x-8 items-center text-blue-800 px-1 pt-1 text-m font-medium ">
                                     {navigation.map((item) => {
@@ -68,35 +60,23 @@ export default function navbar() {
 
                     <Disclosure.Panel className="sm:hidden">
                         <div className="pt-2 pb-4 space-y-1">
-                            {/* Current: "bg-indigo-50 border-indigo-500 text-indigo-700", Default: "border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700" */}
-                            <Disclosure.Button
-                                as="a"
-                                href="#"
-                                className="bg-indigo-50 border-indigo-500 text-indigo-700 block pl-3 pr-4 py-2 border-l-4 text-base font-medium"
-                            >
-                                Dashboard
-                            </Disclosure.Button>
-                            <Disclosure.Button
-                                as="a"
-                                href="#"
-                                className="border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700 block pl-3 pr-4 py-2 border-l-4 text-base font-medium"
-                            >
-                                Team
-                            </Disclosure.Button>
-                            <Disclosure.Button
-                                as="a"
-                                href="#"
-                                className="border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700 block pl-3 pr-4 py-2 border-l-4 text-base font-medium"
-                            >
-                                Projects
-                            </Disclosure.Button>
-                            <Disclosure.Button
-                                as="a"
-                                href="#"
-                                className="border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700 block pl-3 pr-4 py-2 border-l-4 text-base font-medium"
-                            >
-                                Calendar
-                            </Disclosure.Button>
+                            {
+                                navigation.map((item) => {
+                                    const isActive = pathname.endsWith(item.href)
+                                    return (
+                                        <div key={item.name}
+                                             className='border-blue-500 text-blue-700 block pl-3 pr-4 py-2 border-l-4 text-base font-medium'>
+                                            <a
+                                                key={item.name}
+                                                href={item.href}
+                                                className={isActive ? 'border-indigo-500 text-blue-700' : 'border-transparent text-gray-500  hover:text-gray-700 hover:border-gray-300'}
+                                            >
+                                                {item.name}
+                                            </a>
+                                        </div>
+                                    )
+                                })
+                            }
                         </div>
                     </Disclosure.Panel>
                 </>
