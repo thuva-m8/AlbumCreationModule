43f5b9e5d8b6afbcb91c7f65ff9b459f60d2f338
Thuvarakan Mathivannan <thuva@Thuvarakans-iMac.local>
2023-12-14 13:53:42 +0530

    added if conditions in album ui

--- app/album/gallery/[id]/page.js
+++ app/album/gallery/[id]/page.js
@@ -9,32 +9,7 @@ import {generateClient} from "aws-amplify/data";
 
 const client = generateClient()
 
-const products = [
-    {
-        id: '1',
-        imageSrc: 'https://tailwindui.com/img/ecommerce-images/category-page-05-image-card-01.jpg',
-        imageAlt: 'dfdfd'
-    },
-    {
-        id: '2',
-        imageSrc: 'https://tailwindui.com/img/ecommerce-images/category-page-05-image-card-01.jpg',
-        imageAlt: 'dfdfd'
-    },
-    {
-        id: '3',
-        imageSrc: 'https://tailwindui.com/img/ecommerce-images/category-page-05-image-card-01.jpg',
-        imageAlt: 'dfdfd'
-
-
-    },
-    {
-        id: '4',
-        imageSrc: 'https://tailwindui.com/img/ecommerce-images/category-page-05-image-card-01.jpg',
-        imageAlt: 'dfdfd'
-
-
-    },
-]
+const products = []
 
 function classNames(...classes) {
     return classes.filter(Boolean).join(' ')
@@ -63,7 +38,7 @@ export default function AlbumPhotoList() {
     }, [])
     const params = useParams();
     return (
-        <div className="bg-white max-w-7xl mx-auto sm:px-6 lg:px-8 mt-10">
+        <div className="bg-white rounded max-w-7xl mx-auto sm:px-6 lg:px-8 mt-10">
             <Link href="/album">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5"
                      stroke="currentColor" className="w-6 h-6">
@@ -175,23 +150,60 @@ export default function AlbumPhotoList() {
                 {/* Product grid */}
                 <section aria-labelledby="products-heading"
                          className="max-w-7xl mx-auto overflow-hidden sm:px-6 lg:px-8">
-                    <div
-                        className="mx-px border-l border-gray-200 grid grid-cols-2 sm:mx-0 md:grid-cols-3 lg:grid-cols-4">
-                        {products.map((product) => (
-                            <div key={product.id}
-                                 className="group relative p-4 border-r border-b border-gray-200 sm:p-6">
+                    {products.length ?
+                        <div
+                            className="mx-px border-l border-gray-200 grid grid-cols-2 sm:mx-0 md:grid-cols-3 lg:grid-cols-4">
+                            {products.map((product) => (
+                                <div key={product.id}
+                                     className="group relative p-4 border-r border-b border-gray-200 sm:p-6">
+                                    <div
+                                        className=" overflow-hidden bg-gray-200 aspect-w-10 aspect-h-100 group-hover:opacity-90">
+                                        <img
+                                            src={product.imageSrc}
+                                            alt={product.imageAlt}
+                                            className="w-full h-full object-center"
+                                        />
+                                    </div>
+                                </div>
+
+                            ))}
+                        </div>
+                        :
+                        <div>
+                            <div className="mt-6 sm:mt-20 sm:col-span-2 flex justify-center">
                                 <div
-                                    className=" overflow-hidden bg-gray-200 aspect-w-10 aspect-h-100 group-hover:opacity-90">
-                                    <img
-                                        src={product.imageSrc}
-                                        alt={product.imageAlt}
-                                        className="w-full h-full object-center"
-                                    />
+                                    className="max-w-lg flex justify-center px-6 pt-5 pb-6 border-2 border-gray-300 border-dashed rounded-md">
+                                    <div className="space-y-1 text-center">
+                                        <svg
+                                            className="mx-auto h-20 w-12 text-gray-400"
+                                            stroke="currentColor"
+                                            fill="none"
+                                            viewBox="0 0 48 48"
+                                            aria-hidden="true"
+                                        >
+                                            <path
+                                                d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
+                                                strokeWidth={2}
+                                                strokeLinecap="round"
+                                                strokeLinejoin="round"
+                                            />
+                                        </svg>
+                                        <div className="flex text-sm text-gray-600">
+                                            <label
+                                                className="relative cursor-pointer bg-white rounded-md font-medium text-blue-600 hover:text-blue-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-blue-500"
+                                            >
+                                                <span>Upload a Photo</span>
+                                                <input id="file-upload" name="file-upload" type="file"
+                                                       className="sr-only"/>
+                                            </label>
+                                            <p className="pl-1">or drag and drop</p>
+                                        </div>
+                                        <p className="text-xs text-gray-500">PNG, JPG</p>
+                                    </div>
                                 </div>
                             </div>
-
-                        ))}
-                    </div>
+                        </div>
+                    }
                 </section>
 
                 {/* Pagination */}
--- app/album/page.js
+++ app/album/page.js
@@ -73,28 +73,38 @@ export default function Album() {
 
                         {/* Product grid */}
                         <section aria-labelledby="products-heading" className="mt-8">
-
-
-                            <div
-                                className="grid grid-cols-1 gap-y-20 sm:grid-cols-2 gap-x-6 lg:grid-cols-4 xl:gap-x-20">
-                                {album.map((product) => (
-                                    <a key={product.id} href={`album/gallery/${product.id}`} className="group">
-                                        <div
-                                            className="w-full aspect-w-1 aspect-h-6 rounded-lg overflow-hidden sm:aspect-w-2 sm:aspect-h-3">
-                                            <img
-                                                src={product.cover_image_url}
-                                                alt={product.imageAlt}
-                                                className="w-full h-80 object-center object-cover group-hover:opacity-75"
-                                            />
-                                        </div>
-                                        <div
-                                            className="mt-4 flex items-center justify-between ">
-                                            <h1 className='text-sm overflow-hidden whitespace-nowrap truncate font-medium text-gray-900'>{product.name}</h1>
-                                            <p className=' font-medium text-sm text-opacity-30 text-gray-900'>{product.createdAt.toLocaleLowerCase().slice(0, 10)}</p>
-                                        </div>
-                                    </a>
-                                ))}
-                            </div>
+                            {album.length ?
+                                <div
+                                    className="grid grid-cols-1 gap-y-20 sm:grid-cols-2 gap-x-6 lg:grid-cols-4 xl:gap-x-20">
+                                    {album.map((product) => (
+                                        <a key={product.id} href={`album/gallery/${product.id}`} className="group">
+                                            <div
+                                                className="w-full aspect-w-1 aspect-h-6 rounded-lg overflow-hidden sm:aspect-w-2 sm:aspect-h-3">
+                                                <img
+                                                    src={product.cover_image_url}
+                                                    alt={product.imageAlt}
+                                                    className="w-full h-80 object-center object-cover group-hover:opacity-75"
+                                                />
+                                            </div>
+                                            <div
+                                                className="mt-4 flex items-center justify-between ">
+                                                <h1 className='text-sm overflow-hidden whitespace-nowrap truncate font-medium text-gray-900'>{product.name}</h1>
+                                                <p className=' font-medium text-sm text-opacity-30 text-gray-900'>{product.createdAt.toLocaleLowerCase().slice(0, 10)}</p>
+                                            </div>
+                                        </a>
+                                    ))}
+                                </div>
+                                :
+                                <div className="flex justify-center text-l tracking-tight text-red-700">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                         strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+                                        <path strokeLinecap="round" strokeLinejoin="round"
+                                              d="M2.25 15.75l5.159-5.159a2.25 2.25 0 013.182 0l5.159 5.159m-1.5-1.5l1.409-1.409a2.25 2.25 0 013.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 001.5-1.5V6a1.5 1.5 0 00-1.5-1.5H3.75A1.5 1.5 0 002.25 6v12a1.5 1.5 0 001.5 1.5zm10.5-11.25h.008v.008h-.008V8.25zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z"/>
+                                    </svg>
+                                    &nbsp;
+                                    No Available albums
+                                </div>
+                            }
                         </section>
                     </div>
                 </main>
